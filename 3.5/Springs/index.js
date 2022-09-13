98fc63a7b2e0dd3d0acd96138a11117455eea16c
Niranga De Silva <nirangad@gmail.com>
2022-09-13 23:55:45 +0530

    3.5 Springs - Basic motion

--- 3.5/Springs/index.js
+++ 3.5/Springs/index.js
@@ -1,6 +1,11 @@
 let color1, color2, color3;
 let theta, rope, origin;
+let mass;
 let gravity;
+let K;
+
+let position;
+let velocity, acceleration;
 
 function setup() {
   color1 = "#544e61";
@@ -11,9 +16,15 @@ function setup() {
   background(color1);
 
   gravity = 0.8;
+  mass = 2;
   origin = createVector(0, 0);
   rope = 300;
   theta = 0;
+  K = 0.05;
+
+  position = createVector(0, rope + 50);
+  velocity = 0;
+  acceleration = 0;
 }
 
 function draw() {
@@ -24,8 +35,15 @@ function draw() {
 
   translate(width / 2, 0);
 
-  let x = rope * sin(theta);
-  let y = rope * cos(theta);
-  line(origin.x, origin.y, x, y);
-  ellipse(x, y, 30, 30);
+  let f = -K * (position.y - rope);
+  applyForce(f);
+
+  line(origin.x, origin.y, position.x, position.y);
+  ellipse(position.x, position.y, mass * 10, mass * 10);
+}
+
+function applyForce(force) {
+  acceleration = force / mass;
+  velocity += acceleration;
+  position.y += velocity;
 }
