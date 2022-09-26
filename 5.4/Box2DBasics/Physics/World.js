01f6694b337a31222799fad4dc93a7e6e683978a
Niranga De Silva <nirangad@gmail.com>
2022-09-26 23:22:33 +0530

    5.4 Box 2D Basics: Project Set up

---
+++ 5.4/Box2DBasics/Physics/World.js
@@ -0,0 +1,7 @@
+let Matter = require("matter");
+// module aliases
+var Engine = Matter.Engine,
+  Render = Matter.Render,
+  Runner = Matter.Runner,
+  Bodies = Matter.Bodies,
+  Composite = Matter.Composite;
---
+++ 5.4/Box2DBasics/index.js
@@ -0,0 +1,27 @@
+let color1, color2, color3;
+let boxes;
+
+function setup() {
+  color1 = "#a5243d";
+  color2 = "#b48291";
+  color3 = "#b48291";
+
+  createCanvas(700, 700);
+  background(color3);
+  boxes = [];
+}
+
+function draw() {
+  background(color3);
+
+  fill(color1);
+  stroke(color2);
+  boxes.forEach((box) => {
+    box.show();
+  });
+}
+
+function mouseDragged() {
+  let box = new Box(createVector(mouseX, mouseY), { width: 50, height: 50 });
+  boxes.push(box);
+}
